ea120b04d6fe3d499b124a08ec19368ddb3e07ee
J. Antognini <antognini@astronomy.ohio-state.edu>
2014-03-27 19:33:26 -0400

    Added a few constants
    Added G and c and made some redundancies in some of the unit conversions.

--- consts.py
+++ consts.py
@@ -6,17 +6,32 @@
 # A collection of useful astrophysics constants
 #
 
+# Physical constants
+G = 6.67384e-11
+c = 299792458
 hubble_constant = 70.4 # km / s / Mpc
 speed_of_light_km_s = 299792.458 # km / s
+
+# Masses
 solarmass2kg = 1.989e30
+solarmass = 1.989e30
+
+# Distances
 parsec2m = 3.0857e16
+parsec = 3.0857e16
 au2m = 1.496e11
-year2sec = 3.155693e7
+au = 1.496e11
 rsun2m = 6.9599e8
 mpc2m = 3.0857e22
 kpc2m = 3.0857e19
 m2mpc = 1 / mpc2m
 m2kpc = 1 / kpc2m
+
+# Times
+year2sec = 3.155693e7
+year = 3.155693e7
+
+# Angular conversions
 radian2arcsec = 206265.
 arcsec2radian = 1 / radian2arcsec
 radian2arcmin = 3437.75
