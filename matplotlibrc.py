050e593d47b0b9df35b89d5c517b19100c591155
J. Antognini <antognini@astronomy.ohio-state.edu>
2014-04-16 13:34:07 -0400

    Added a matplotlibrc file
    This new file is a collection of default settings for using matplotlib

---
+++ matplotlibrc.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+#
+# Some default parameters for matplotlib
+#
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.axes as ax
+
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.serif'] = ['Computer Modern Roman']
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.size'] = 30
+
