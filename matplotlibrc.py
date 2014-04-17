f742b975cee80f21baa4a55c2432f614ea011f65
J. Antognini <antognini@astronomy.ohio-state.edu>
2014-04-17 14:02:32 -0400

    Added larger tick sizes

--- matplotlibrc.py
+++ matplotlibrc.py
@@ -13,3 +13,11 @@ mpl.rcParams['font.serif'] = ['Computer Modern Roman']
 mpl.rcParams['text.usetex'] = True
 mpl.rcParams['font.size'] = 30
 
+mpl.rcParams['xtick.major.size'] = 10
+mpl.rcParams['xtick.minor.size'] = 5
+mpl.rcParams['ytick.major.size'] = 10
+mpl.rcParams['ytick.minor.size'] = 5
+
+plt.axes().set_aspect(1)
+
+# TODO: Set default edgecolor to none
