9b378f85345f5afbf710179cff23d582dabfde00
J. Antognini <antognini@astronomy.ohio-state.edu>
2015-07-22 19:20:31 -0400

    Minor changes
    - Changed tick length
    - Formatting

--- matplotlibrc.py
+++ matplotlibrc.py
@@ -11,13 +11,15 @@ import matplotlib.axes as ax
 mpl.rcParams['font.family'] = 'serif'
 mpl.rcParams['font.serif'] = ['Computer Modern Roman']
 mpl.rcParams['text.usetex'] = True
-mpl.rcParams['font.size'] = 30
+mpl.rcParams['font.size'] = 28
 
 mpl.rcParams['xtick.major.size'] = 10
 mpl.rcParams['xtick.minor.size'] = 5
 mpl.rcParams['ytick.major.size'] = 10
 mpl.rcParams['ytick.minor.size'] = 5
 
+
+mpl.rcParams['xtick.major.pad']='6'
 #mpl.rcParams['legend.scatterpoints'] = 1
 
 plt.axes().set_aspect(1)
